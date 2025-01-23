e7e498bafbf31f00b19c00f86ccce7ed38f4c22d
srfl0-admin <e1121549@u.nus.edu>
2025-01-23 13:21:58 +0800

    Increment level-0

---
+++ src/main/java/Ixo.java
@@ -0,0 +1,11 @@
+public class Ixo {
+    public static void main(String[] args){
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Ixo!");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
