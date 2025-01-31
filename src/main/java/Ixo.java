eaf6f4dfa47f2ecf1161a3ff3f0cbd01733d6587
srfl0-admin <e1121549@u.nus.edu>
2025-01-31 17:04:32 +0800

    Add functionalities to store user inputs and repeat them upon user request
    Add comments to clarify code

--- src/main/java/Ixo.java
+++ src/main/java/Ixo.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Ixo {
 
-    public static String sepa = "____________________________________________________________";
+    public static String separator = "____________________________________________________________";
 
-    public static void echo(){
+    public static void echo() {
         System.out.println("I'll now repeat after you! Type 'bye' by itself to make me stop.");
 
         String[] line;
         Scanner echoScan;
-        String space;
+
+        String space; //variable to determine if spacing is needed for purposes of echoing
 
         while (true) {
             echoScan = new Scanner(System.in);
@@ -21,38 +22,97 @@ public class Ixo {
                 return;
             }
 
+            System.out.println();
+
             System.out.print("I say: ");
             for (String word : line) {
                 System.out.print(word);
                 space = (word.equals(line[line.length - 1])) ? "" : " ";
                 System.out.print(space);
             }
-            System.out.println();
+            System.out.println(separator);
         }
 
     }
 
-    public static void command(){
+    public static void add() {
+        System.out.println("I'll now take a note on what you typed. Type 'bye' by itself to make me stop.");
+
+        String[] line;
+        Scanner addScan;
+
+        String[] store = new String[50]; //limit list to store only 50 items
+        int storeIndex = 0;
+        String newline;
+
+        String space; //variable to determine if spacing is needed for purposes of echoing
+
+        while (true) {
+            addScan = new Scanner(System.in);
+            System.out.print("You said: ");
+            newline = addScan.nextLine();
+            line = newline.split(" ");
+
+            switch (line[0]) {
+            case "bye":
+                return;
+
+            case "list":
+                int i = 1;
+                for (String item : store) {
+                    if (item == null){break;}
+                    System.out.println(i + ". " + item);
+                    i++;
+                }
+                continue;
+            }
+
+            System.out.println();
+
+            System.out.print("added: ");
+            for (String word : line) {
+                System.out.print(word);
+                space = (word.equals(line[line.length - 1])) ? "" : " ";
+                System.out.print(space);
+            }
+            store[storeIndex] = newline;
+            storeIndex++;
+            System.out.println();
+            System.out.println(separator);
+        }
+
+    }
+
+    public static void command() {
         String cmd;
         Scanner cmdScan = new Scanner(System.in);
         cmd = cmdScan.nextLine();
-        if (cmd.equalsIgnoreCase("echo")){
+        switch (cmd.toLowerCase()) {
+        case "echo":
             echo();
+            break;
+
+        case "add":
+            add();
+            break;
+
+            
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println(sepa);
+        System.out.println(separator);
         System.out.println("Hello! I'm Ixo!");
         System.out.println("What can I do for you?");
         System.out.println("1. Echo");
-        System.out.println(sepa);
+        System.out.println("2. Add");
+        System.out.println(separator);
 
         Ixo.command();
 
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(sepa);
+        System.out.println(separator);
 
     }
 }
