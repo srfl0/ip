63ae2b0639b5165a15f743d980751b6b44979bc3
srfl0-admin <e1121549@u.nus.edu>
2025-01-31 15:33:13 +0800

    Add interfacing for commands
    Add echo() command to chatbot

--- src/main/java/Ixo.java
+++ src/main/java/Ixo.java
@@ -1,11 +1,58 @@
+import java.util.Scanner;
+
 public class Ixo {
+
+    public static String sepa = "____________________________________________________________";
+
+    public static void echo(){
+        System.out.println("I'll now repeat after you! Type 'bye' by itself to make me stop.");
+
+        String[] line;
+        Scanner echoScan;
+        String space;
+
+        while (true) {
+            echoScan = new Scanner(System.in);
+            System.out.print("You said: ");
+            line = echoScan.nextLine().split(" ");
+
+            if (line[0].equals("bye")) {
+                System.out.println("Okay, goodbye!");
+                return;
+            }
+
+            System.out.print("I say: ");
+            for (String word : line) {
+                System.out.print(word);
+                space = (word.equals(line[line.length - 1])) ? "" : " ";
+                System.out.print(space);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void command(){
+        String cmd;
+        Scanner cmdScan = new Scanner(System.in);
+        cmd = cmdScan.nextLine();
+        if (cmd.equalsIgnoreCase("echo")){
+            echo();
+        }
+    }
+
     public static void main(String[] args){
-        String line = "____________________________________________________________";
-        System.out.println(line);
+
+        System.out.println(sepa);
         System.out.println("Hello! I'm Ixo!");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println("1. Echo");
+        System.out.println(sepa);
+
+        Ixo.command();
+
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(sepa);
+
     }
 }
